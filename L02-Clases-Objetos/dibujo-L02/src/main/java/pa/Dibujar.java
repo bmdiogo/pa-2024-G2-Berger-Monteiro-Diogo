c9955196a106eaf062cbd7576fca4b313166a88b
Diogo Berger Monteiro <diogoberger@outlook.com>
2023-09-29 10:45:19 +0200

    dibujo

--- L02-Clases-Objetos/dibujo-L02/src/main/java/pa/Dibujar.java
+++ L02-Clases-Objetos/dibujo-L02/src/main/java/pa/Dibujar.java
@@ -13,11 +13,43 @@ public class Dibujar {
      */
     public static void main(String[] args) {
         //String s
-    	Circle sol;
+    	Circle sol;//def de la variable se pot poner Circle sol = new Circle();
+    	Square azul= new Square();
+    	Square verde =new Square();
+    	Square rosa = new Square();
     	sol=new Circle();
+    	Triangle rojo=new Triangle();
+    	Triangle rojoo=new Triangle();
+
+    	
+    	azul.changeColor("blue");
+    	azul.moveHorizontal(-230);
+    	azul.moveVertical(40);
+    	
+    	verde.changeColor("green");
+    	verde.moveHorizontal(60);
+    	verde.moveVertical(40);
+    	
+    	sol.moveVertical(-60);
+    	
+    	rojo.moveHorizontal(-100);
+    	rojo.moveVertical(-35);
+    	
+    	rojoo.moveHorizontal(190);
+    	rojoo.moveVertical(-35);
+    	
+    	rosa.changeColor("magenta");
+    	rosa.moveHorizontal(-230);
+       
+    	
+    	
     	sol.makeVisible();
-    			
-    			
+    	azul.makeVisible();
+    	verde.makeVisible();
+    	rojo.makeVisible();
+    	rojoo.makeVisible();
+    	rosa.makeVisible();
+
     	System.out.println("\n\n Ejercicio pendiente de realizar \n\n");
         
     }
--- L02-Clases-Objetos/dibujo-L02/src/main/java/pa/Triangle.java
+++ L02-Clases-Objetos/dibujo-L02/src/main/java/pa/Triangle.java
@@ -27,7 +27,7 @@ public class Triangle
         width = 70;
         xPosition = 210;
         yPosition = 140;
-        color = "green";
+        color = "red";
         isVisible = false;
     }
 
