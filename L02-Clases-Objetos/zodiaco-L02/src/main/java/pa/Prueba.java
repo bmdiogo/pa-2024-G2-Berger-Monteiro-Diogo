04a13f42588b47fd2911babc83a75544d003b1e7
Diogo Berger Monteiro <diogoberger@outlook.com>
2023-10-04 11:30:48 +0200

    Terminado ej(proyecto zodiacoo) de L02

--- L02-Clases-Objetos/zodiaco-L02/src/main/java/pa/Prueba.java
+++ L02-Clases-Objetos/zodiaco-L02/src/main/java/pa/Prueba.java
@@ -8,7 +8,18 @@ public class Prueba {
      */
     public static void main(String[] args) {
     	
-    	System.out.println("\n\n Ejercicio pendiente de realizar \n\n");
- 
+    	Zodiaco fecha1= new Zodiaco(23,9);
+    	Zodiaco fecha2= new Zodiaco(9,40);
+        Zodiaco fecha3= new Zodiaco(-3,8);
+        Zodiaco fecha4= new Zodiaco(23,11);
+        Zodiaco fecha5= new Zodiaco(21,1);
+        Zodiaco fecha6= new Zodiaco(23,8);
+        System.out.println(fecha1.getDia() +","+fecha1.getMes()+"---->"+fecha1.obtener_signo());
+        System.out.println(fecha2.getDia() +","+fecha2.getMes()+"---->"+fecha2.obtener_signo());
+        System.out.println(fecha3.getDia() +","+fecha3.getMes()+"---->"+fecha3.obtener_signo());
+        System.out.println(fecha4.getDia() +","+fecha4.getMes()+"---->"+fecha4.obtener_signo());
+        System.out.println(fecha5.getDia() +","+fecha5.getMes()+"---->"+fecha5.obtener_signo());
+        System.out.println(fecha6.getDia() +","+fecha6.getMes()+"---->"+fecha6.obtener_signo());
+
     }
 }    
--- L02-Clases-Objetos/zodiaco-L02/src/main/java/pa/Zodiaco.java
+++ L02-Clases-Objetos/zodiaco-L02/src/main/java/pa/Zodiaco.java
@@ -26,15 +26,145 @@ public class Zodiaco {
        falso en caso contrario   
     */  
     private boolean fecha_valida() {
+    	
+    	switch (mes) {
+    	case 1,3,5,7,8,10,12:
+    		if (dia>0 && dia<=31) {
+     		   return true;
+     	   }
+     	   else {
+     		   return false;
+     		   }
+    	
+    	case 2:
+    		if(dia>0 && dia<29) {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    	case 4,6,9,11:
+    		if (dia>0 && dia<=30) {
+    			return true;
+    		}
+    		else {
+    			return false;
+    		}
+    		}
+		return false;
+    	
+    	
         
-       return false;
+       
     }
  
     /**
      * Devuelve el nombre del signo asociado al día y mes de nacimiento, o el mensaje "Fecha inválida"   
     */
     public String obtener_signo() {
-        
-        return "";
-    }    
+    	if (fecha_valida()) {
+    	switch(mes) {
+    	case 1:
+			if (dia<21) {
+				signo="Capricornio";
+			}
+			else {
+				signo="Acuario";
+			}
+			break;
+		case 2:
+			if (dia<20) {
+				signo="Acuario";
+			}
+			else {
+				signo="Piscis";
+			}
+			break;
+		case 3:
+			if (dia<21) {
+				signo="Piscis";
+			}
+			else {
+				signo="Aries";
+			}
+			break;
+		case 4:
+			if (dia<21) {
+				signo="Aries";
+			}
+			else {
+				signo="Tauro";
+			}
+			break;
+		case 5:
+			if (dia<22) {
+				signo="Tauro";
+			}
+			else {
+				signo="Géminis";
+			}
+			break;
+		case 6:
+			if (dia<22) {
+				signo="Géminis";
+			}
+			else {
+				signo="Cáncer";
+			}
+			break;
+		case 7:
+			if (dia<24) {
+				signo="Cáncer";
+			}
+			else {
+				signo="Leo";
+			}
+			break;
+		case 8:
+			if (dia<24) {
+				signo="Leo";
+			}
+			else {
+				signo="Virgo";
+			}
+			break;
+		case 9:
+			if (dia<24) {
+				signo="Virgo";
+			}
+			else {
+				signo="Libra";
+			}
+			break;
+		case 10:
+			if (dia<24) {
+				signo="Libra";
+			}
+			else {
+				signo="Escorpio";
+			}
+			break;
+		case 11:
+			if (dia<23) {
+				signo="Escorpio";
+			}
+			else {
+				signo="Sagitario";
+			}
+			break;
+		case 12:
+			if (dia<22) {
+				signo="Sagitario";
+			}
+			else {
+				signo="Capricornio";
+			}
+			break;
+	}
+	}
+else {
+	signo="Fecha inválida";
+}
+return signo;
+}    
 }
